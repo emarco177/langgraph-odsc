86b3d62824bbc950dbd7a505946b68c74a61cec9
Eden Marco <emarco177@gmail.com>
2024-09-04 12:07:05 +0300

    added critique loop

--- graph4/chains.py
+++ graph4/chains.py
@@ -1,14 +1,35 @@
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
+
 
 reflection_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
-            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+            """
+                        You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet.
+                        Always provide detailed recommendations, including requests for length, virality, style, etc.,
+                        "Always return your answer in markdown format
+
+                        ```markdown
+                        **Original Tweet:** Just finished my first day back at work after vacation. Feeling kinda blah
+                        
+                        Answer:
+                        **Grade:** 5/10
+
+                        **Critique:** This tweet is a bit too generic. It lacks a hook to grab attention and doesn't really give us any insight
+                        into your vacation or how you're feeling.
+
+                        **Recommendations:**
+                        - **Length:** Keep it under 280 characters for maximum impact.
+                        - **Virality:** Try adding a question or call to action to encourage engagement. For example, "What's the best thing
+                        you did on your last vacation?"
+                        - **Style:** Use more descriptive language to paint a picture for your followers. Instead of 'kinda blah,' try
+                        something like 'overwhelmed by the return to reality' or 'missing the sunshine and cocktails.'
+            
+                        **Original Tweet:**: {tweet}
+            """,
         ),
-        MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
@@ -16,12 +37,15 @@ generation_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a twitter techie influencer assistant tasked with revising twitter posts according to feedback."
-            "Revise the current tweet to the best twitter post possible."
-            "You should respond with a revised version of the tweet."
-            "Revised tweet: ",
+            "You are a twitter techie influencer assistant tasked with revising twitter posts according"
+            " to feedback from the previous message ."
+            "Revise the current tweet according te critique to the best twitter post possible."
+            "{critique}"
+            "You ONLY should respond with a revised version of the tweet."
+            "**current tweet:** {tweet}"
+            "There is not need for markdown format, only write the tweet and do not write the critique"
+            "**New Tweet:** ",
         ),
-        MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
--- graph4/main.py
+++ graph4/main.py
@@ -1,57 +1,74 @@
-from typing import List, Sequence
+import operator
+from typing import TypedDict, Annotated, Dict, Any
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.graph import END, MessageGraph
+from langgraph.graph import END, StateGraph
 
 from chains import generate_chain, reflect_chain
+
+
+class State(TypedDict):
+    messages: Annotated[list, operator.add]
+    tweet: str
+    original_tweet: str
+    critique: str
+    revised_tweet: str
+    iterations: int
 
 
 REFLECT = "reflect"
 GENERATE = "generate"
 
 
-def generation_node(state: Sequence[BaseMessage]):
-    return generate_chain.invoke({"messages": state})
+def generation_node(state: State) -> Dict[str, Any]:
+    res = generate_chain.invoke(
+        {"tweet": state["tweet"], "critique": state["critique"]}
+    )
+    return {"revised_tweet": res.content, "iterations": int(state["iterations"]) + 1}
 
 
-def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = reflect_chain.invoke({"messages": messages})
-    return [HumanMessage(content=res.content)]
+def reflection_node(state: State) -> Dict[str, Any]:
+    res = reflect_chain.invoke({"tweet": state["tweet"]})
+    return {
+        "critique": res.content,
+    }
 
 
-builder = MessageGraph()
-builder.add_node(GENERATE, generation_node)
+builder = StateGraph(State)
 builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(REFLECT)
 
+builder.add_node(GENERATE, generation_node)
 
-def should_continue(state: List[BaseMessage]):
-    if len(state) > 4:
+
+def should_continue(state: Dict[str, Any]):
+    if int(state["iterations"]) >= 2:
         return END
-    return REFLECT
+    return GENERATE
 
 
 builder.add_conditional_edges(REFLECT, should_continue)
 builder.add_edge(GENERATE, REFLECT)
 
 graph = builder.compile()
-graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+# graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 if __name__ == "__main__":
     print("Hello LangGraph")
-    inputs = HumanMessage(
-        content="""Tweet:
-                                    @LangChainAI
-            — newly Tool Calling feature is seriously underrated.
+    tweet = """
+            
+            @LangChainAI
+            — newly Tool Calling feature is underrated.
+            After a long wait, it's  here- making the implementation of agents across different models
+            with function calling - super easy.
+    """
 
-            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
-                                  """
-    )
-    response = graph.invoke(inputs)
-    print(response)
-    # for state in graph.stream(inputs, {"configurable": {"thread_id": "foo"}}, stream_mode="values"):
-    #     print(f"{state=}")
+    for state in graph.stream(
+        {"tweet": tweet, "revised_tweet": tweet, "iterations": 0},
+        {"configurable": {"thread_id": "odsc_userid_12844"}},
+        stream_mode="values",
+    ):
+        print(f"{state=}")
